1f63eb928b85415414f7de0a2588133d4852c2ea
bradyward <114321616+bradyward@users.noreply.github.com>
2025-04-14 02:05:42 +0000

    Working version

--- src/lab.c
+++ src/lab.c
@@ -14,6 +14,11 @@
 #include <errno.h>
 #endif
 
+// Fix to map_anonymous error
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS 0x20
+#endif
+
 #include "lab.h"
 
 #define handle_error_and_die(msg) \
@@ -44,54 +49,113 @@ size_t btok(size_t bytes)
 
 struct avail *buddy_calc(struct buddy_pool *pool, struct avail *buddy)
 {
-    /** Buddy pointer math: 
-        p_base = 0x10000000
-        p_current_block = 0x1000F100
-        offset = p_current_block - p_base = 0x0000F100
-        offset_buddy = offset xor (1<<k) = 0x0000F000
-        buddy_pointer = base + offset_buddy = 0x1000F000
-     */
-    size_t buddy_size = UINT64_C(1) << buddy->kval; 
+    size_t buddy_size = UINT64_C(1) << buddy->kval;
     // Find the an offset to transition into a local frame
-    size_t offset = &buddy - (size_t)pool->base;
+    uintptr_t offset = (uintptr_t)buddy - (uintptr_t)pool->base;
     // Get the local offset of the buddy
-    size_t offset_buddy = offset ^ buddy_size;
+    uintptr_t offset_buddy = offset ^ buddy_size;
     // Get the buddy's address back into a global frame
-    struct avail *buddy_pointer = (struct avail*)((size_t)pool->base + offset_buddy);
-    return buddy_pointer;
+    return (struct avail *)((uintptr_t)pool->base + offset_buddy);
 }
 
 void *buddy_malloc(struct buddy_pool *pool, size_t size)
 {
-    if (pool == NULL || size == 0) {
+    if (pool == NULL || size == 0)
+    {
         return NULL;
     }
 
     // get the kval for the requested size with enough room for the tag and kval fields
-    size_t requested_k = btok(size + UINT64_C(2) * sizeof(unsigned short int));
+    size_t requested_k = btok(size + sizeof(struct avail));
 
     // R1 Find a block
-    int index = UINT16_C(0);
-    while (pool->avail[index].tag != BLOCK_AVAIL && pool->avail[index].kval < requested_k && index < MAX_K - UINT64_C(1)) {
+    int index = requested_k;
+    while (index <= pool->kval_m && pool->avail[index].next == &pool->avail[index])
+    {
         index++;
     }
 
     // There was not enough memory to satisfy the request thus we need to set error and return NULL
-    if (pool->avail[index].tag == BLOCK_RESERVED || pool->avail[index].kval < requested_k) {
+    if (index > pool->kval_m)
+    {
         errno = ENOMEM;
-        return 0;
+        return NULL;
     }
 
     // R2 Remove from list;
-    // dll remove
+    struct avail *returned_block = pool->avail[index].next;
+    struct avail *next_block = returned_block->next;
+
+    // Disconnect the block we intend to return
+    pool->avail[index].next = next_block;
+    next_block->prev = &pool->avail[index];
+
+    // Update data of allocated block
+    returned_block->tag = BLOCK_RESERVED;
+    returned_block->kval = index;
 
     // R3 Split required?
+    while (index > requested_k)
+    { // Continue to split block until it is the correct size
+        index--;
 
-    // R4 Split the block
+        // R4: Split block
+        uintptr_t addr = (uintptr_t)returned_block;
+        uintptr_t buddy_addr = addr + (1UL << index);
+
+        struct avail *buddy = (struct avail *)buddy_addr;
+        buddy->tag = BLOCK_AVAIL;
+        buddy->kval = index;
+
+        // Insert buddy into the avail[index] list
+        buddy->next = pool->avail[index].next;
+        buddy->prev = &pool->avail[index];
+        pool->avail[index].next->prev = buddy;
+        pool->avail[index].next = buddy;
+
+        returned_block->kval = index;
+    }
+    return (void *)(returned_block + UINT64_C(1));
 }
 
 void buddy_free(struct buddy_pool *pool, void *ptr)
 {
+    if (ptr == NULL)
+    {
+        return NULL;
+    }
+
+    struct avail *free_ptr = (struct avail *)((char *)ptr - sizeof(struct avail)); // move full sizeof(struct avail) back instead of 2 short int
+    struct avail *buddy_ptr = buddy_calc(pool, free_ptr);
+
+    // Merge block back with its buddy as long as much as possible
+    while (free_ptr->kval < pool->kval_m && buddy_ptr->tag == BLOCK_AVAIL && buddy_ptr->kval == free_ptr->kval)
+    {
+        // Remove the buddy from the available blocks
+        buddy_ptr->prev->next = buddy_ptr->next;
+        buddy_ptr->next->prev = buddy_ptr->prev;
+
+        // Merge the two blocks
+        if (buddy_ptr < free_ptr)
+        {
+            free_ptr = buddy_ptr;
+        }
+
+        // Update new block's size and attempt to find its buddy
+        free_ptr->kval++;
+        buddy_ptr = buddy_calc(pool, free_ptr);
+    }
+
+    // Mark final block as available and return it to the avail list
+    free_ptr->tag = BLOCK_AVAIL;
+    size_t kval = free_ptr->kval;
+    struct avail *head = &pool->avail[kval];
+
+    // Attach it to a block in the avail list
+    free_ptr->next = head->next;
+    free_ptr->prev = head;
+    head->next->prev = free_ptr;
+    head->next = free_ptr;
 }
 
 /**
